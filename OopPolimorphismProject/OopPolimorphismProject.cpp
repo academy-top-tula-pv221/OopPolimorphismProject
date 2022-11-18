d20c0b44e8ca926e6fda51b2089e687b761375ff
efimov-top-academy <efimov_m@t.top-academy.ru>
2022-11-18 20:09:33 +0300

    18-11-22

--- OopPolimorphismProject/OopPolimorphismProject.cpp
+++ OopPolimorphismProject/OopPolimorphismProject.cpp
@@ -1,5 +1,6 @@
 ﻿#include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -9,6 +10,7 @@ protected:
     string type;
 public:
     Transport(string type = "") : type{type} {}
+    
     virtual string Move()
     {
         return type + " move";
@@ -20,9 +22,15 @@ class Car : public Transport
     int speed;
 public:
     Car(string type, int speed) : Transport(type), speed{ speed } {}
-    string Move()
+    
+    string Move() override
     {
         return Transport::Move() + " with speed: " + to_string(speed);
+    }
+
+    void CarMethod()
+    {
+        cout << "Hello car\n";
     }
 };
 
@@ -31,26 +39,35 @@ class Plane : public Transport
     int height;
 public:
     Plane(string type, int height) : Transport(type), height{ height } {}
-    string Move()
+    
+    string Move() override
     {
         return Transport::Move() + " at height: " + to_string(height);
     }
 };
 
+class PoliceCar : public Car
+{
+public:
+    PoliceCar(string type, int speed) : Car(type, speed) {}
+
+    string Move() override
+    {
+        return Car::Move() + " ua-ua-ua";
+    }
+};
+
 int main()
 {
-    Transport* transport = new Transport("transport");
-    Car* car = new Car("car", 200);
-    Plane* plane = new Plane("plane", 10000);
-
-    cout << car->Move() << "\n";
-    cout << plane->Move() << "\n";
-
     Transport* transports[3];
-    transports[0] = transport;
-    transports[1] = car;
-    transports[2] = plane;
+    transports[0] = new Transport("transport");
+    transports[1] = new Car("car", 200);
+    transports[2] = new Plane("plane", 10000);
+    
 
     for (int i = 0; i < 3; i++)
         cout << transports[i]->Move() << "\n";
+
+    PoliceCar pcar("police car", 190);
+    cout << pcar.Move();
 }
